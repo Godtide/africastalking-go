e2eee8a0fd6358d0ce7ccc509f1e1874745837ce
Juma Allan <allanjuma@gmail.com>
2020-05-14 12:25:09 +0300

    setting up structs on models

--- airtime/airtime.go
+++ airtime/airtime.go
@@ -11,25 +11,6 @@ import (
 	"time"
 )
 
-// Response is the reponse from the api
-type Response struct {
-	NumSent       int
-	TotalAmount   string
-	TotalDiscount string
-	ErrorMessage  string
-	Responses     []Entry
-}
-
-// Entry is the entry for each airtime response
-type Entry struct {
-	ErrorMessage string
-	PhoneNumber  string
-	Amount       string
-	Discount     string
-	Status       string
-	RequestID    string
-}
-
 // Service is the airtime service
 type Service struct {
 	Username string
