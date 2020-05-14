370549abe1ce304cad5452bb3c9882b962cc06fd
Juma Allan <allanjuma@gmail.com>
2020-05-14 12:19:11 +0300

    setting up send airtime func

--- airtime/airtime.go
+++ airtime/airtime.go
@@ -2,6 +2,7 @@ package airtime
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/AndroidStudyOpenSource/africastalking-go/util"
 	"net/http"
@@ -68,6 +69,28 @@ func (service Service) Send() (*Response, error) {
 	var airtimeResponse Response
 	json.NewDecoder(response.Body).Decode(&airtimeResponse)
 	return &airtimeResponse, nil
+}
+
+func (service Service) SendAirtime(phoneNumber, amount string) (*Response, error) {
+	values := url.Values{}
+	values.Set("username", service.Username)
+	values.Set("phoneNumber", phoneNumber)
+	values.Set("amount", amount)
+
+	smsURL := util.GetSmsURL(service.Env)
+	headers := make(map[string]string)
+	headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+	res, err := service.newPostRequest(smsURL, values, headers)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var response Response
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return nil, errors.New("unable to parse sms response")
+	}
+	return &response, nil
 }
 
 func (service Service) newPostRequest(url string, values url.Values, headers map[string]string) (*http.Response, error) {
